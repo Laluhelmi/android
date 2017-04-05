dc577cf51e8b219e7e76f73f66f07d9c3eb8dd6b
Laluhelmi <lhilmifadli@yahoo.co.id>
2017-04-05 22:00:39 +0700

    tt

--- app/src/main/java/com/example/l/afiefbelajar/coba.java
+++ app/src/main/java/com/example/l/afiefbelajar/coba.java
@@ -1,5 +1,4 @@
 package com.example.l.afiefbelajar;
-
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 public class coba extends AppCompatActivity {
 
     @Override
@@ -24,6 +32,27 @@ public class coba extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,"", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONArray jsonArray = new JSONArray(response);
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject object = jsonArray.getJSONObject(i);
+
+                    }
+                }catch (Exception e){
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
     }
 
 }
